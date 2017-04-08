5fa2d84b23c694df855f038a569a6b1acdcc62b6
yujuan <15809216911@163.com>
2017-04-08 17:05:10 +0800

    Modify the routers

--- controllers/animals.js
+++ controllers/animals.js
@@ -1,3 +1,17 @@
 /**
  * Created by yujuan on 17-4-8.
  */
+var express = require('express')
+    , router = express.Router();
+
+// Domestic animals page
+router.get('/domestic', function(req, res) {
+  res.send('Cow, Horse, Sheep')
+});
+
+// Wild animals page
+router.get('/wild', function(req, res) {
+  res.send('Wolf, Fox, Eagle')
+});
+
+module.exports = router;
--- controllers/cars.js
+++ controllers/cars.js
@@ -1,3 +1,17 @@
 /**
  * Created by yujuan on 17-4-8.
  */
+var express = require('express');
+var router = express.Router();
+
+router.get('/brands', (req, res)=> {
+  res.send("Audi");
+});
+
+router.get('/models', (req, res)=> {
+  res.send("Q7");
+});
+
+module.exports = router;
+
+
