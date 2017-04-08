261a6dd5d77cc0333049bfbcaa339d7f219a2485
yujuan <15809216911@163.com>
2017-04-08 17:04:25 +0800

    Modify the index.js which is responsible for loading all controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,14 @@
 /**
  * Created by yujuan on 17-4-8.
  */
+var express = require('express');
+var router = express.Router();
+
+router.use('/cars', require('./cars'));
+router.use('/animals', require('./animals'));
+
+router.get('/',(req,res)=>{
+  res.send("this is homePage");
+});
+
+module.exports = router;
